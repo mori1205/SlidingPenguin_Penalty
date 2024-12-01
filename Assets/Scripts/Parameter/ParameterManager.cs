eecfe291582489cd595bc8061e89a6d4e2a38a3e
mori1205 <mm2384mm@gm.tsuda.ac.jp>
2024-12-01 18:29:31 +0900

    加速度変更

--- Assets/Scripts/Parameter/ParameterManager.cs
+++ Assets/Scripts/Parameter/ParameterManager.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class ParameterManager : MonoBehaviour
 {
   // game parameters
-  public static float sensitivity = 3.0f;
+  public static float sensitivity = 0.05f; //3.0
   public static int limitedTime = 30;
   public static float maximumSpeed = 3.0f; //15
   public static float acceleration = 0.050f;
